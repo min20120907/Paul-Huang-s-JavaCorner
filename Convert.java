f19cd9416d3a1bc056a0d84088d0607df276ddc2
Cangyu <jefflin.je598@gmail.com>
2021-03-15 16:40:31 +0800

    update homeworks

---
+++ Convert.java
@@ -0,0 +1,8 @@
+public class Convert {
+	public static void main(String args[]) {
+		double pound = 0.454;
+		System.out.println("10 kg = "+10/pound+"pb");
+		System.out.println("50 kg = "+50/pound+"pb");
+		System.out.println("100 kg = "+100/pound+"pb");
+	}
+}
