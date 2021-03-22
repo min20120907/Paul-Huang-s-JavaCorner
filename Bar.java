3f378ce525753bf2a6260d16a88c720a69b64a8a
Cangyu <jefflin.je598@gmail.com>
2021-03-22 14:48:47 +0800

    Add notes about the basic interface concept

--- Bar.java
+++ Bar.java
@@ -1,3 +1,28 @@
+interface ExampleInterface {
+	public abstract void methodB();
+	// final void methodG() {	// Final is not allowed in interfaces
+	// 	System.out.println("G");
+	// 
+	// }
+	// Private modifier is not allowed in interfaces
+	// 
+	// private abstract void methodC();
+	public String methodD();
+	// 
+	// The variable should be predefined
+	// public int x; 
+	//
+	// Final is not allowed
+	// 
+	// final void methodE();
+	// 
+	// cant have body
+	// 
+	// public void methodF(){
+	// 	System.out.println("F");
+	// }
+}
+
 class Foo {
 	private void print() {
 		System.out.println("Bonjour le monde!");
