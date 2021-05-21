a2afaec8da3a8253a1004d8fc990149d69b4c58c
Cangyu <jefflin.je598@gmail.com>
2021-05-21 09:50:20 +0800

    update  network

--- Network/EchoClient.java
+++ Network/EchoClient.java
@@ -1,2 +1,34 @@
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketAddress;
+
 public class EchoClient{
+    public static void main(String[] args) {
+        try{
+            if (args.length<3){
+                System.err.println("<msg> <addr> <port>");
+                System.exit(1);
+            }
+            //args msg, host, port
+            String msg = args[0];
+            InetAddress addr = InetAddress.getByName(args[1]);
+            int port = Integer.parseInt(args[2]);
+            byte[] msgbytes = msg.getBytes();
+            DatagramPacket pkt = new DatagramPacket(msgbytes, msgbytes.length);
+            DatagramSocket sck = new DatagramSocket();
+            sck.send(pkt);
+            byte[] buf = new byte[256];
+            DatagramPacket rpkt = new DatagramPacket(buf, buf.length)
+            sck.setSoTimeout(3000);
+            sck.receive(rpkt);
+            System.out.println(msg);
+        } catch (SocketTimeoutException ex){
+            System.err.println("ReadTimeout");
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
 }
--- Network/EchoServer.java
+++ Network/EchoServer.java
@@ -1,26 +1,37 @@
 import java.net.DatagramSocket;
 import java.net.SocketAddress;
 import java.net.SocketException;
+import java.io.IOException;
 import java.net.DatagramPacket;
+import java.net.SocketAddress;
+
 public class EchoServer {
 	public final int service_port;
-	public EchoServer(){
+
+	public EchoServer() {
 		this.service_port = 5454;
 	}
+
 	public EchoServer(int port) {
 		this.service_port = port;
 	}
-	public void startRunning() throws SocketException{
+
+	public void startRunning() throws SocketException, IOException {
 		DatagramSocket srvsock = new DatagramSocket(service_port);
 		byte[] buf = new byte[256];
-		while(true){
+		while (true) {
 			DatagramPacket rpkt = new DatagramPacket(buf, 256);
 			srvsock.receive(rpkt);
 			SocketAddress rmt_addr = rpkt.getSocketAddress();
 			String rmsg = new String(rpkt.getData(), 0, rpkt.getLength());
-			String smeg = "GOT: "+rmsg;
-			DatagramPacket spkt = new DatagramPacket();
-
+			String smsg = "GOT: " + rmsg;
+			byte[] sbytes = smsg.getBytes();
+			DatagramPacket spkt = new DatagramPacket(sbytes, sbytes.length);
+			srvsock.send(spkt);
 		}
+	}
+
+	public static void main(String[] args) {
+		
 	}
 }
